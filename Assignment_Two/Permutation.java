176d9ddbbe50573907adffc1e051b0ce2362f1e5
Scott Hammer <scott.t.hammer@gmail.com>
2022-11-20 01:15:44 -0500

    Modified check for when input is empty, and output

--- Assignment_Two/Permutation.java
+++ Assignment_Two/Permutation.java
@@ -4,11 +4,11 @@ import edu.princeton.cs.algs4.StdIn;
 public class Permutation {
     public static void main(String[] args) {
         RandomizedQueue<Object> test = new RandomizedQueue<>();
-        while(!StdIn.readString().isEmpty()) {
+        while(!StdIn.isEmpty()) {
             test.enqueue(StdIn.readString());
         }
-
-        while(!test.isEmpty()) {
+        int input = Integer.parseInt(args[0]);
+        for(int i = 0; i < input; i++) {
             System.out.println(test.dequeue());
         }
     }
